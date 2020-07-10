fd4ebef1a49e7b05c8dd3ccac72fb410a13c43c4
vigneshrajaraman <53133803+vigneshrajaraman@users.noreply.github.com>
2020-07-10 21:12:05 +0530

    commited

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   constructor(private fb:FormBuilder) { }
-
+falg=false;
   form;
   ngOnInit(): void {
     this.form=this.fb.group({
